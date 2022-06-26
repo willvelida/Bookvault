3356ea77ba19a0f447b326270aafff697ee3c3e5
Will Velida <willvelida@microsoft.com>
2022-06-26 20:12:02 +1200

    Change from Book Id to Id

--- src/Bookvault.Web/Bookvault.Web/Data/Book.cs
+++ src/Bookvault.Web/Bookvault.Web/Data/Book.cs
@@ -2,7 +2,7 @@
 {
     public class Book
     {
-        public string BookId { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Category { get; set; }
